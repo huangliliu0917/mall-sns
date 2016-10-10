f29a0c2d2906868a731bf6896e6f7859d590dc92
jin346952524 <346952524@qq.com>
2016-10-10 14:13:58 +0800

    添加用户接口

---
+++ src/main/java/com/huotu/huobanplus/sns/exception/ConcernException.java
@@ -0,0 +1,20 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+package com.huotu.huobanplus.sns.exception;
+
+/**
+ * Created by jin on 2016/10/10.
+ * 关注异常
+ */
+public class ConcernException extends Exception {
+    public ConcernException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/huotu/huobanplus/sns/repository/UserCircleRepository.java
@@ -0,0 +1,27 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+package com.huotu.huobanplus.sns.repository;
+
+import com.huotu.huobanplus.sns.entity.Circle;
+import com.huotu.huobanplus.sns.entity.User;
+import com.huotu.huobanplus.sns.entity.UserCircle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by jin on 2016/10/10.
+ */
+@Repository
+public interface UserCircleRepository extends JpaRepository<UserCircle, Long>, JpaSpecificationExecutor<UserCircle> {
+
+    UserCircle findByUserAndCircle(@Param("user") User user, @Param("circle") Circle circle);
+}
