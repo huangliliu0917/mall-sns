ec0fd8ca426218182300756a848deba9a701bcbd
lgh <106517651@qq.com>
2016-10-11 14:30:32 +0800

    后台目录及前端

---
+++ src/main/java/com/huotu/huobanplus/sns/controller/admin/AdminManagerController.java
@@ -0,0 +1,12 @@
+package com.huotu.huobanplus.sns.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+@Controller
+@RequestMapping("/top/manager")
+public class AdminManagerController {
+}
